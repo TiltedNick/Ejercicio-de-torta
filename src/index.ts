47c7a96ec576994d0beeaa9d62049147e1b6472a
TiltedNick <104039871+TiltedNick@users.noreply.github.com>
2022-04-26 19:34:56 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,94 @@
+// How to tiramisú
+// Ingredientes
+let Sponsor: string = "@LuxuryDeliciasRG";
+let Tiramisu1: string = "café instantaneo";
+let Tiramisu2: string = "vainillas";
+let Tiramisu3: string = "crema batida";
+let Tiramisu4: string = "queso neutro";
+let Tiramisu5: string = "azucar impalpable";
+let Tiramisu6: string = "cacao";
+let Tiramisu7: string = "ralladura de limon";
+let Tiramisu8: string = "crema de leche";
+let Tiramisu9: string = "ralladura de chocolate";
+// Medidas
+let Medida1: number = 350;
+let Medida2: string = "un cuarto";
+
+// Preparación
+console.log("Para empezar, lo primero que hay que preparar es el " + Tiramisu1);
+console.log(
+  "La medida para el mismo son 2 cucharadas soperas por taza de " + Tiramisu1
+);
+console.log(
+  "El siguiente paso seria empezar con la preparacion de la " +
+    Tiramisu3 +
+    "." +
+    "Para esto necesitaremos una batidora y un bowl relativamente grande"
+);
+console.log(
+  "En el bowl vertir un pote de " +
+    Medida1 +
+    " gramos de " +
+    Tiramisu8 +
+    " (preferentemente con un tenor graso por encima del 20% para acelerar el tiempo de batida)"
+);
+console.log(
+  "A continuacion batir la " +
+    Tiramisu8 +
+    " por aproximadamente 30 segundos y agregar " +
+    Medida1 / 2 +
+    " gramos de " +
+    Tiramisu4 +
+    " y aproximadamente " +
+    Medida2 +
+    " de taza de " +
+    Tiramisu5 +
+    "." +
+    " En caso de preferir una preparacion mas dulce agregar " +
+    Tiramisu5 +
+    " a gusto"
+);
+console.log(
+  "Para terminar la preparacion de la crema agregar un poco de " +
+    Tiramisu7 +
+    " y resumir la batida hasta conseguir una textura mas espesa."
+);
+console.log(
+  "Una vez terminada la " +
+    Tiramisu3 +
+    " preparar el recipiente para comenzar con el armado del Tiramisú"
+);
+console.log(
+  "Para comenzar con el armado, lo primero es remojar brevemente las " +
+    Tiramisu2 +
+    " en el " +
+    Tiramisu1 +
+    " previamente mencionado y empezar a cubrir la base del recipiente con las mismas."
+);
+console.log(
+  "Una vez creado el primer piso de " +
+    Tiramisu2 +
+    " es necesario agregar la " +
+    Tiramisu3 +
+    " de manera que cubra las " +
+    Tiramisu2 +
+    " totalmente y asi crear nuestra primera capa de nuestro misu"
+);
+console.log(
+  "Repetir este ultimo paso las veces que sea necesario para llenar el recipiente o hasta quedarse sin " +
+    Tiramisu2 +
+    " o " +
+    Tiramisu3 +
+    "."
+);
+console.log(
+  "Por ultimo esparcir un poco de " +
+    Tiramisu6 +
+    " o " +
+    Tiramisu9 +
+    " hasta cubrir la superficie de crema que deberia haber quedado arriba del todo. "
+);
+console.log(
+  "En caso de no tener los ingredientes o las ganas de preparar un tiramisú no te hagas problema porque eso se soluciona contactando al sponsor de esta receta en 🡳"
+);
+console.log(Sponsor + " ;)");
